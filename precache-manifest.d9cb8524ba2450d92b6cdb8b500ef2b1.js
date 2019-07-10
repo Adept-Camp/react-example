ebed56f12675126b228298bf18f0b6af8f36f218
Julien Genestoux <julien.genestoux@gmail.com>
2019-07-10 23:36:30 +0200

    Updates

---
+++ precache-manifest.d9cb8524ba2450d92b6cdb8b500ef2b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5793de060e356d3b839d964b5f18e5c4",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "43764c06e59d2b3f0987",
+    "url": "/react-example/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "672d1fcac7d4e1d95f50",
+    "url": "/react-example/static/js/2.d3ba7428.chunk.js"
+  },
+  {
+    "revision": "43764c06e59d2b3f0987",
+    "url": "/react-example/static/js/main.03dd5cb7.chunk.js"
+  },
+  {
+    "revision": "fffe8ea8f4a6c0c7c1e3",
+    "url": "/react-example/static/js/runtime~main.c7bd7e12.js"
+  }
+]);
